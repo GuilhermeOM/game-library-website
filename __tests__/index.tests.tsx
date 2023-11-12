8330a31345e23a0f4a8c39af72c9eeb90c2d50e9
Guilherme <guilherme.omalta0@gmail.com>
2023-11-11 21:29:51 -0300

    Tested NavigateButton, checking if component is navigating to the correct path after clicking and checking if component is rendering the given text correctly.

--- __tests__/index.tests.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-it('test if test is working', () => {
-  const { getByText } = render(<h1>test</h1>);
-
-  expect(getByText('test')).toBeInTheDocument();
-});
