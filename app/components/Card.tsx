91726b3118011618c4ff17f6389ad40564a8ca86
Guilherme <guilherme.omalta0@gmail.com>
2023-12-02 16:10:54 -0300

    Redone GenreCardList, fixed twitch streams cards border on hover and recreated genresResume to a json file.

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 export default function Card({ children, className = '' }: CardProps) {
   return (
     <div
-      className={`flex w-full h-32 p-4 pb-8 gap-4 rounded-md border border-stone-900 bg-stone-950 relative ${className}`}
+      className={`flex w-full h-32 p-4 gap-4 rounded-md border border-stone-900 bg-stone-950 relative ${className}`}
     >
       {children}
     </div>
